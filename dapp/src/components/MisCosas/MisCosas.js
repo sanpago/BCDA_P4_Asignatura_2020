37f77ccd754e8e9ff70d135e8a9ba8d66f50dd52
Santiago Pavon <santiago.pavon@upm.es>
2021-09-30 22:56:58 +0200

    Implementar Mis Cosas.

---
+++ dapp/src/components/MisCosas/MisCosas.js
@@ -0,0 +1,14 @@
+import MisDatos from "./MisDatos";
+import MisNotas from "./MisNotas";
+
+const MisCosas = () => {
+
+    return <section className="AppMisCosas">
+        <h2>Mis Cosas</h2>
+        <MisDatos/>
+        <MisNotas/>
+    </section>;
+}
+
+export default MisCosas;
+
