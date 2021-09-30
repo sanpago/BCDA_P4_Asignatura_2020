3adb50710ac1efb01bf945431a4b8ee482c20d3a
Santiago Pavon <santiago.pavon@upm.es>
2021-09-30 22:43:05 +0200

    Añadidos componentes DrizzleReact a Header.

--- dapp/src/components/Header.js
+++ dapp/src/components/Header.js
@@ -1,10 +1,34 @@
+import {drizzleReactHooks} from '@drizzle/react-plugin'
+import {newContextComponents} from "@drizzle/react-components";
 
-const Header = () => (
-    <header className="AppHeader">
-        <h1>
-            Asignatura
-        </h1>
-    </header>
-);
+const {ContractData} = newContextComponents;
+const {useDrizzle, useDrizzleState} = drizzleReactHooks;
+
+const Header = () => {
+    const {drizzle} = useDrizzle();
+    const drizzleState = useDrizzleState(state => state);
+
+    return (
+        <header className="AppHeader">
+            <h1>
+                Asignatura: &nbsp;
+                <ContractData
+                    drizzle={drizzle}
+                    drizzleState={drizzleState}
+                    contract={"Asignatura"}
+                    method={"nombre"}
+                />
+                -
+                <ContractData
+                    drizzle={drizzle}
+                    drizzleState={drizzleState}
+                    contract={"Asignatura"}
+                    method={"curso"}
+                    render={data => ( <em>{data}</em> )}
+                />
+            </h1>
+        </header>
+    );
+};
 
 export default Header;
